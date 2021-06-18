b72a5f5c4014adfc1e6cbe38a30f58cfd5ce4ee7
laurent <ladardenne@orange.fr>
2021-06-18 18:54:46 +0200

    Modifie le nom du module

--- src/Demo/Find-SCCMBreakingChange.ps1
+++ src/Demo/Find-SCCMBreakingChange.ps1
@@ -8,12 +8,14 @@ $CSharpPathToScan=@(
 
 $Files=@(
   ($PowershellPathToScan|Get-ChildItem -include *.ps1,*.psm1 -Recurse -File)
-  ('…\Src'|Get-ChildItem -include *.ps1,*.psm1 -File)
+  ('…todo...\Src'|Get-ChildItem -include *.ps1,*.psm1 -File)
   ($CSharpPathToScan|Get-ChildItem -include *.cs -Recurse -File|Where-Object {$_.DirectoryName -notmatch '^Bin$|^Obj$'})
 )
 
-Import-Module '..\Tools\Modules\SCCMLibraryChanges\SCCMLibraryChanges.psd1' -force
+Import-Module SCCMCheckCmdletChanges -force
 #Find-CommandName -PathToScan $Files.FullName
 $R=Find-CommandName -PathToScan $Files.FullName
+
+#todo localiser le délimiteur
 $R|Sort-Object Version,Name,Path|Export-Csv C:\Temp\Release.csv -Delimiter ';' -NoTypeInformation
 Invoke-Item C:\Temp\Release.csv
