847cfb6b19b1cd7ff31a8afb8d76198bc98edbfd
laurent <ladardenne@orange.fr>
2021-06-23 20:08:55 +0200

    Simplification du code.

--- src/Demo/Find-SCCMBreakingChange.ps1
+++ src/Demo/Find-SCCMBreakingChange.ps1
@@ -1,21 +1,18 @@
+#Requires -Module SCCMCheckCmdletChanges
+
 $PowershellPathToScan=@(
-'..\Src\ \Modules'
+'.\Examples'
 )
 
 $CSharpPathToScan=@(
-  '..\Deploiement'
+  '.\Examples'
 )
 
 $Files=@(
   ($PowershellPathToScan|Get-ChildItem -include *.ps1,*.psm1 -Recurse -File)
-  ('…todo...\Src'|Get-ChildItem -include *.ps1,*.psm1 -File)
   ($CSharpPathToScan|Get-ChildItem -include *.cs -Recurse -File|Where-Object {$_.DirectoryName -notmatch '^Bin$|^Obj$'})
 )
 
 Import-Module SCCMCheckCmdletChanges -force
-#Find-CommandName -PathToScan $Files.FullName
-$R=Find-CommandName -PathToScan $Files.FullName
 
-#todo localiser le délimiteur
-$R|Sort-Object Version,Name,Path|Export-Csv C:\Temp\Release.csv -Delimiter ';' -NoTypeInformation
-Invoke-Item C:\Temp\Release.csv
+Find-CommandName -Path $Files.FullName
