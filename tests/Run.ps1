c7f5962a87ccb814494dcfb600191e4cad21ffe2
Laurent Dardenne <LaDardenne@orange.fr>
2022-12-14 12:56:17 +0100

    Remove comment

--- tests/Run.ps1
+++ tests/Run.ps1
@@ -5,5 +5,4 @@ $Configuration = New-PesterConfiguration
 
 $Configuration.Output.Verbosity = ('Detailed')
 
-#The response time of a call to Find-Module on PSGallery can be degraded (between 30 and 60 seconds), where Myget responds between 3 and 6 seconds.
 Invoke-Pester -Configuration $Configuration
